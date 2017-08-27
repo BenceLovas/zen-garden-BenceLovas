d970519e1a39308f5cfe8f0b4998e8144866d392
Bence Lovas <bansoul1@gmail.com>
2017-08-27 20:27:35 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+
+$(document).ready(function () {
+    $("ul").hide();
+    $(".plus-design").click( function () {
+        $(".design-selection ul").slideToggle(1000);
+    });
+});
+
+$(document).ready(function () {
+    $("ul").hide();
+    $(".plus-archives").click( function () {
+        $(".design-archives ul").slideToggle(1000);
+    });
+});
+
+$(document).ready(function () {
+    $("ul").hide();
+    $(".plus-resources").click( function () {
+        $(".zen-resources ul").slideToggle(1000);
+    });
+});
+
+$(document).ready(function () {
+    $(".main p").hide();
+    $(".plus-explanation").click( function () {
+        $(".explanation p").slideToggle(1000);
+    });
+});
+
+$(document).ready(function () {
+    $(".main p").hide();
+    $(".plus-participation").click( function () {
+        $(".participation p").slideToggle(1000);
+    });
+});
+
+$(document).ready(function () {
+    $(".main p").hide();
+    $(".plus-benefits").click( function () {
+        $(".benefits p").slideToggle(1000);
+    });
+});
+
+$(document).ready(function () {
+    $(".main p").hide();
+    $(".plus-requirements").click( function () {
+        $(".requirements p").slideToggle(1000);
+    });
+});
